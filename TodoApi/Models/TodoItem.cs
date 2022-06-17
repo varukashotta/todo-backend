6d07cfccf622f36960e6828244bf5ee7d0bdcbb2
Albert Deda <alkashotta@gmail.com>
2022-06-17 14:02:45 +0800

    To do

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace TodoApi.Models
+{
+    public abstract class BaseEntity
+    {
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+
+    public class TodoItem: BaseEntity
+	{
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
+
---
+++ TodoApi/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors(b => b.AllowAnyOrigin().AllowAnyHeader());
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
